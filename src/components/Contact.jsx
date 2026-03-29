adc77c38a380d7a2f72361bea64edef7af672ef8
dinesh@03 <dineshmathan03@gmail.com>
2026-03-29 11:12:10 +0530

    Change background color of message input field

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -88,7 +88,7 @@ export default function Contact() {
                   required
                   rows={5}
                   placeholder="Hello, I'd like to talk about..."
-                  style={{ width: "100%", padding: "12px 16px", border: "1px solid #f3f5f7", borderRadius: 8, background: "#000000", resize: "vertical" }}
+                 style={{ width: "100%", padding: "12px 16px", border: "1px solid #f3f5f7", borderRadius: 8, background: "#faf7f7", resize: "vertical" }}
                 />
               </div>
 
